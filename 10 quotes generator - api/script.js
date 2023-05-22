760dae20600d80962c04ca66ca5f12db43cf54d4
Oliwer Kirkiewicz <oliwer.kirkiewicz@gmail.com>
2023-05-22 18:42:59 +0200

    22-05

---
+++ 10 quotes generator - api/script.js
@@ -0,0 +1,14 @@
+const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
+console.log(fetch(url))
+
+
+
+
+// const text = document.querySelector('.text');
+// const btn = document.querySelector('button');
+// const atr = document.querySelector('.author');
+
+// btn.addEventListener('click', e => {
+//     text.textContent = 'somethingrandom'
+//     atr.textContent = 'randombatuh'
+// })
