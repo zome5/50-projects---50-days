e536ef49e8ace7c400260e55123f4e75b3080500
Oliwer Kirkiewicz <oliwer.kirkiewicz@gmail.com>
2023-05-23 19:18:05 +0200

    23-05

--- 10 quotes generator - api/script.js
+++ 10 quotes generator - api/script.js
@@ -1,14 +1,34 @@
-const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
-console.log(fetch(url))
+const text = document.querySelector('.text');
+const btn = document.querySelector('button');
+const atr = document.querySelector('.author');
+const div = document.querySelector('.quote')
+const bad = document.querySelector('.bad');
+
+const randWord = ['poor', 'shitty', 'awful', 'cheap', 'sad', 'unacceptable', 'off', 'not good', 'garbage', 'crappy']
 
 
+function generateJoke() {
+    bad.textContent = randWord[Math.floor(Math.random() * 10)]
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
 
+    fetch('https://v2.jokeapi.dev/joke/Dark', config)
+        .then(response => response.json())
+        .then(data => {
 
-// const text = document.querySelector('.text');
-// const btn = document.querySelector('button');
-// const atr = document.querySelector('.author');
+            if (data.setup === undefined) {
+                text.innerHTML = data.joke;
+                atr.innerHTML = ""
+            } else {
+                text.innerHTML = data.setup
+                atr.innerHTML = data.delivery
 
-// btn.addEventListener('click', e => {
-//     text.textContent = 'somethingrandom'
-//     atr.textContent = 'randombatuh'
-// })
+            }
+        })
+}
+
+div.addEventListener('click', generateJoke)
+// div.addEventListener('click', e => bad.textContent = randWord[Math.floor(Math.random() * 12)])
