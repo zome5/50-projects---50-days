7a557a9d242db971aeaee3ed894f4dfc9e2aec24
Oliwer Kirkiewicz <oliwer.kirkiewicz@gmail.com>
2023-05-27 11:30:00 +0200

    27-05

--- 12 random choice picker/script.js
+++ 12 random choice picker/script.js
@@ -20,43 +20,55 @@ window.addEventListener('keydown', e => {
         }
 
         spans = document.querySelectorAll('span')
+        pickRandom();
+        if (spans.length > 0) setTimeout(() => createButton(), 1000)
     }
 })
 
-pickRandom();
+
 
 function pickRandom() {
+    const len = 15;
 
-    const len = spans.length
-
-    setTimeout(() => {
-        for (let x = 0; x < len; x++) {
-
-            const randPick = Math.floor(Math.random() * spans.length);
+    for (let x = 0; x < len; x++) {
+        const randPick = Math.floor(Math.random() * spans.length);
+        if (x < len / 3) {
             setTimeout(() => {
-                spans[randPick].style.backgroundColor = "#fff"
-            }, 300)
+                spans[randPick].style.backgroundColor = '#fff';
+                setTimeout(() => {
+                    spans[randPick].style.backgroundColor = 'rgb(123, 61, 131)';
+                }, 25);
+            }, x * 50)
+        } else if (x >= len / 3) {
             setTimeout(() => {
-                spans[randPick].style.backgroundColor = 'rgb(123, 61, 131)'
-            }, 350)
+                spans[randPick].style.backgroundColor = '#fff';
+                setTimeout(() => {
+                    spans[randPick].style.backgroundColor = 'rgb(123, 61, 131)';
+                }, 100);
+            }, x * 150)
+        } else {
+            setTimeout(() => {
+                spans[randPick].style.backgroundColor = '#fff';
+                setTimeout(() => {
+                    spans[randPick].style.backgroundColor = 'rgb(123, 61, 131)';
+                }, 200);
+            }, x * 300)
         }
-
-    }, 1)
-
+    }
+    setTimeout(() => {
+        spans[Math.floor(Math.random() * spans.length)].style.backgroundColor = '#fff'
+    }, (50 * len / 3 + 25) + (150 * len / 3 + 100) + (300 * len / 3 + 200))
 }
 
+function createButton() {
 
-// if (i <= len) {
-//     spans[i].style.backgroundColor = "white"
-//     setTimeout(spans[i].style.backgroundColor = 'rgb(123, 61, 131)', 200)
-// } else if (i > len) {
-//     spans[i].style.backgroundColor = "white"
-//     spans[i].style.backgroundColor = 'rgb(123, 61, 131)'
-// } else if (i > 2 * len) {
-//     spans[i].style.backgroundColor = "white"
-//     spans[i].style.backgroundColor = 'rgb(123, 61, 131)'
-// } else {
-//     spans[i].style.backgroundColor = "white"
-//     spans[i].style.backgroundColor = 'rgb(123, 61, 131)'
-// }
-// }
+    const btn = document.createElement('button');
+    btn.textContent = 'CLEAR';
+    btn.classList.add('clear');
+    choices.appendChild(btn)
+
+    btn.addEventListener('click', e => {
+        spans.forEach(span => span.remove())
+        choices.removeChild(btn);
+    })
+}
